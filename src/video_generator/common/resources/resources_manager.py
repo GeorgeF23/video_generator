bd65306b9b818d6ffa7bf2103fca85638a4ef493
George Floarea <floareageorge@gmail.com>
2022-07-24 21:50:49 +0300

    Created a youtube downloading method

---
+++ src/video_generator/common/resources/resources_manager.py
@@ -0,0 +1,35 @@
+import logging
+import os
+from environment import tmp_dir
+from subprocess import run
+from uuid import uuid4
+
+def download_youtube_resource(url: str) -> str:
+    logging.info(f'[youtube downloading] Starting downloading of {url}')
+    local_filename = uuid4().hex
+    local_path = os.path.join(tmp_dir, local_filename)
+
+
+    yt_dl = os.environ.get('YOUTUBE_DL_PATH')
+    timeout = int(os.environ.get('DOWNLOAD_TIMEOUT'))
+    command = f'{yt_dl} -o {local_path} {url}'
+
+    logging.debug(f'[youtube downloading] Command is: {command} with timeout {timeout}')
+
+    p = run(command, shell=True, timeout=timeout)
+
+    if p.returncode != 0:
+        return None
+
+    return local_path + '.webm'
+
+def download_resource(url: str, youtube = False) -> str:
+    if youtube == True:
+        local_resource = download_youtube_resource(url)
+
+    if not os.path.exists(local_resource):
+        logging.error('[Resource download] Local resource could not be found after download')
+        raise RuntimeError('Could not download resource')
+
+    logging.info(f'[Resource download] Downloaded {url} to {local_resource}')
+    return local_resource
--- src/video_generator/main.py
+++ src/video_generator/main.py
@@ -7,6 +7,8 @@ load_dotenv()
 from reddit.RedditClient import RedditClient
 from text2speech.SpeechClient import SpeechClient
 import logging
+from environment import initialize_environment
+from common.resources.resources_manager import download_resource
 logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
 
 
@@ -18,4 +20,7 @@ logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
 # es_client = ElasticSearchClient.get_instance()
 # es_client.upload(posts, lambda post: post.id, 'reddit_posts')
 
-print(SpeechClient.get_instance().get_text_to_speech("My boyfriend(M20)and me(F19) have been dating for 7 months,and I’ve noticed some weird interactions between him and his animals. As we started our relationship I’ve noticed he shows a lot of attention to his cats,like kisses and laying down with them as he would with me or calling them his girlfriends.We were talking on the phone one night and he was telling me about his female cats(he has two)we weren’t really talking that much until he said”you wanna know something I noticed about Jenny?”I replied yeah what is it?he said “Jenny doesn’t like when someone else touches her private parts,but when I do it she doesn’t mind”."))
+initialize_environment()
+download_resource('https://www.youtube.com/watch?v=qu8X8UxBjjM', youtube=True)
+
+# print(SpeechClient.get_instance().get_text_to_speech("My boyfriend(M20)and me(F19) have been dating for 7 months,and I’ve noticed some weird interactions between him and his animals. As we started our relationship I’ve noticed he shows a lot of attention to his cats,like kisses and laying down with them as he would with me or calling them his girlfriends.We were talking on the phone one night and he was telling me about his female cats(he has two)we weren’t really talking that much until he said”you wanna know something I noticed about Jenny?”I replied yeah what is it?he said “Jenny doesn’t like when someone else touches her private parts,but when I do it she doesn’t mind”."))

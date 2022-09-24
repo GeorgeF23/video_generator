30ad8b1e3e2c4013cd45215e1875020f8fed10ee
George Floarea <floareageorge@gmail.com>
2022-09-24 11:49:54 +0300

    Made s3 client more flexible

--- src/video_generator/common/s3/S3Client.py
+++ src/video_generator/common/s3/S3Client.py
@@ -5,32 +5,33 @@ from urllib.parse import urlparse
 
 
 class S3Client():
-	def __init__(self, bucket_name: str | None = None, upload_dir: str = 'tmp'):
-		"""Creates a new instance of the S3 Client
+	instance = None
 
-		Args:
-			bucket_name (str | None, optional): The bucket to connect to. If not provided, then the value in the S3_BUCKET env var will be used.
-			upload_dir (str | None, optional): The default upload directory to be used. Defaults to 'tmp'
+	@staticmethod
+	def get_instance():
+		if not S3Client.instance:
+			S3Client.instance = S3Client()
+		return S3Client.instance
+
+	def __init__(self):
+		"""Creates a new instance of the S3 Client
 
 		Raises:
 			err: Raised an error if the connection fails.
 		"""
 		try:
-			if not bucket_name:
-				bucket_name = os.environ.get('S3_BUCKET', '')
-			self.bucket_name = bucket_name
-			self.upload_dir = upload_dir
 			self.s3_client = boto3.Session().client('s3')
 		except Exception as err:
 			logging.error(f'There was an error while connecting to the S3 service: {err}')
 			raise err
 
-	def upload_file(self, local_path: str, s3_key: str | None = None) -> str:
+	def upload_file(self, local_path: str, s3_bucket: str | None = None, s3_key: str | None = None) -> str:
 		"""Uploads a file to s3
 
 		Args:
 			local_path (str): path to the file to upload
-			s3_key (str | None, optional): The path to upload to. If None, then it will upload to the upload_dir passed to S3Client. Defaults to None.
+			s3_bucket (str | None, optional): Bucket to upload to. Defaults to S3_BUCKET env var.
+			s3_key (str | None, optional): The path to upload to. Defaults to 'tmp/{filename}'.
 
 		Raises:
 			err: Raises an error if the upload fails
@@ -39,16 +40,18 @@ class S3Client():
 			str: Returns the s3 url of the remote file.
 		"""
 		try:
+			if not s3_bucket:
+				s3_bucket = os.environ.get('S3_BUCKET', 'not defined')
 			if not s3_key:
-				s3_key = os.path.join(self.upload_dir, local_path)
-			logging.debug(f'Uploading file from {local_path} to bucket: {self.bucket_name} key: {s3_key}')
+				s3_key = 'tmp/' + local_path.split('/')[-1]
+			logging.debug(f'Uploading file from {local_path} to bucket: {s3_bucket} key: {s3_key}')
 			
 			self.s3_client.upload_file(
 				Filename=local_path,
-				Bucket=self.bucket_name,
+				Bucket=s3_bucket,
 				Key=s3_key
 			)
-			return f's3://{self.bucket_name}/{s3_key}'
+			return f's3://{s3_bucket}/{s3_key}'
 		except Exception as err:
 			logging.error(f'There was an error while uploading file {local_path} to {s3_key}: {err}')
 			raise err
@@ -67,15 +70,19 @@ class S3Client():
 			str: Path of the local file)
 		"""
 		try:
-			s3_key = urlparse(s3_url).path[1:] if s3_url.startswith('s3://') else s3_url
-			local_tmp_dir = os.environ.get('TMP_DIR', 'tmp')
-			filename = s3_key.split('/')[-1]
-			local_path = os.path.join(local_tmp_dir, filename)
+			parsed_url = urlparse(s3_url) 
+			s3_key = parsed_url.path[1:]
+			s3_bucket = parsed_url.hostname
 
-			logging.debug(f'Downloading file from bucket: {self.bucket_name} key: {s3_key} to {local_path}')
+			if not local_path:
+				local_tmp_dir = os.environ.get('TMP_DIR', 'tmp')
+				filename = s3_key.split('/')[-1]
+				local_path = os.path.join(local_tmp_dir, filename)
+
+			logging.debug(f'Downloading file from bucket: {s3_bucket} key: {s3_key} to {local_path}')
 			self.s3_client.download_file(
 				Key=s3_key,
-				Bucket=self.bucket_name,
+				Bucket=s3_bucket,
 				Filename=local_path
 			)
 			return local_path
